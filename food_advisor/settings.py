70759829d15c9c4452fb3ca27dd0537f08dbd43e
mykola <mykolabiron@gmail.com>
2025-09-26 09:04:56 +0200

    fix css

--- food_advisor/settings.py
+++ food_advisor/settings.py
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
